e812ed370a39e53dd7ed442f4eaced369ae911d7
Viktor Lidholt <viktorlidholt@gmail.com>
2023-06-28 15:22:31 +0200

    More real-time communication slides.

--- lib/src/slides/realtime_communications.dart
+++ lib/src/slides/realtime_communications.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:fluttercon_serverpod/src/slides/shared.dart';
 import 'package:slick_slides/slick_slides.dart';
 
+const _chapterTitle = 'Real-time communication';
+const _connectionLifecycle = 'Server-side lifecycle';
+const _clientConnections = 'Connecting from client';
+
 const _codeBasicStructure = '''class ExampleEndpoint extends Endpoint {
   @override
   Future<void> streamOpened(StreamingSession session) async {
@@ -19,6 +23,66 @@ const _codeBasicStructure = '''class ExampleEndpoint extends Endpoint {
   }
 }''';
 
+const _codeStreamOpened = '''
+Future<void> streamOpened(StreamingSession session) async {
+  sendStreamMessage(session, myFullState);
+}''';
+
+const _codeStreamOpenedFull = '''
+Future<void> streamOpened(StreamingSession session) async {
+  sendStreamMessage(session, myFullState);
+
+  session.messages.addListener('myChannel', (message) {
+    sendStreamMessage(session, message);
+  });
+}''';
+
+const _codeHandleStreamMessage = '''
+Future<void> streamOpened(StreamingSession session) async {
+  sendStreamMessage(session, myFullState);
+
+  session.messages.addListener('myChannel', (message) {
+    sendStreamMessage(session, message);
+  });
+}
+
+Future<void> handleStreamMessage(
+  StreamingSession session,
+  SerializableEntity message,
+) async {
+  if (message is IncrementalStateUpdate) {
+    session.messages.send('myChannel', message);
+  }
+}''';
+
+const _codeClientConnection = '''
+Future<void> listenToUpdates() async {
+  await for (var message in client.example.stream) {
+    if (update is FullStateUpdate) {
+      // Handle full state update.
+    }
+    else if (update is IncrementalStateUpdate) {
+      // Handle incremental update.
+    }
+  }
+}''';
+
+const _codeClientConnectionSend = '''
+Future<void> listenToUpdates() async {
+  await for (var message in client.example.stream) {
+    if (update is FullStateUpdate) {
+      // Handle full state update.
+    }
+    else if (update is IncrementalStateUpdate) {
+      // Handle incremental update.
+    }
+  }
+}
+
+void sendMessage(IncrementalStateUpdate message) async {
+  await client.example.sendStreamMessage(message);
+}''';
+
 final realtimeCommunicationsSlides = [
   Slide(
     transition: defaultTransition,
@@ -31,8 +95,8 @@ final realtimeCommunicationsSlides = [
   Slide(
     transition: defaultTransition,
     builder: (context) => const ContentSlide(
-      title: Text('Serverpod'),
-      subtitle: Text('Serializing objects'),
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
       content: Align(
         alignment: Alignment.topLeft,
         child: ColoredCode(
@@ -44,8 +108,8 @@ final realtimeCommunicationsSlides = [
   Slide(
     transition: crossfadeTransistion,
     builder: (context) => const ContentSlide(
-      title: Text('Serverpod'),
-      subtitle: Text('Serializing objects'),
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
       content: Align(
         alignment: Alignment.topLeft,
         child: ColoredCode(
@@ -58,8 +122,8 @@ final realtimeCommunicationsSlides = [
   Slide(
     transition: crossfadeTransistion,
     builder: (context) => const ContentSlide(
-      title: Text('Serverpod'),
-      subtitle: Text('Serializing objects'),
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
       content: Align(
         alignment: Alignment.topLeft,
         child: ColoredCode(
@@ -72,13 +136,82 @@ final realtimeCommunicationsSlides = [
   Slide(
     transition: crossfadeTransistion,
     builder: (context) => const ContentSlide(
-      title: Text('Serverpod'),
-      subtitle: Text('Serializing objects'),
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
       content: Align(
         alignment: Alignment.topLeft,
         child: ColoredCode(
           code: _codeBasicStructure,
           highlightedLines: [12, 13, 14],
+        ),
+      ),
+    ),
+  ),
+  Slide(
+    transition: defaultTransition,
+    builder: (context) => const ContentSlide(
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
+      content: Align(
+        alignment: Alignment.topLeft,
+        child: ColoredCode(
+          code: _codeStreamOpened,
+        ),
+      ),
+    ),
+  ),
+  Slide(
+    builder: (context) => const ContentSlide(
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
+      content: Align(
+        alignment: Alignment.topLeft,
+        child: ColoredCode(
+          code: _codeStreamOpenedFull,
+          animateFromCode: _codeStreamOpened,
+        ),
+      ),
+    ),
+  ),
+  Slide(
+    builder: (context) => const ContentSlide(
+      title: Text(_chapterTitle),
+      subtitle: Text(_connectionLifecycle),
+      content: Align(
+        alignment: Alignment.topLeft,
+        child: ColoredCode(
+          code: _codeHandleStreamMessage,
+          animateFromCode: _codeStreamOpenedFull,
+        ),
+      ),
+    ),
+  ),
+  Slide(
+    transition: defaultTransition,
+    builder: (context) => const ContentSlide(
+      title: Text(_chapterTitle),
+      subtitle: Text(_clientConnections),
+      content: Align(
+        alignment: Alignment.topLeft,
+        child: ColoredCode(
+          code: _codeClientConnection,
+          highlightedLines: [1, 2, 3, 4, 5, 6, 7, 8],
+          animateHighlightedLines: true,
+        ),
+      ),
+    ),
+  ),
+  Slide(
+    builder: (context) => const ContentSlide(
+      title: Text(_chapterTitle),
+      subtitle: Text(_clientConnections),
+      content: Align(
+        alignment: Alignment.topLeft,
+        child: ColoredCode(
+          code: _codeClientConnectionSend,
+          animateFromCode: _codeClientConnection,
+          highlightedLines: [12],
+          animateHighlightedLines: true,
         ),
       ),
     ),
