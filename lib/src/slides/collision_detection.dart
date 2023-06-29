c35e1376c98696330334cc8997fff98a07fa5cea
Viktor Lidholt <viktorlidholt@gmail.com>
2023-06-29 12:00:24 +0200

    Fixed collision detection slides

--- lib/src/slides/collision_detection.dart
+++ lib/src/slides/collision_detection.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttercon_serverpod/src/quadtree/quadtree.dart';
 import 'package:fluttercon_serverpod/src/slides/shared.dart';
 import 'package:slick_slides/slick_slides.dart';
+import 'package:spritewidget/spritewidget.dart';
 
 const _chapterTitle = 'Collision detection';
 
@@ -49,9 +52,49 @@ final collisionDetectionSlides = [
   Slide(
     transition: defaultTransition,
     builder: (context) {
+      return ContentSlide(
+        content: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: SpriteWidget(naiveCollisionView10),
+        ),
+      );
+    },
+  ),
+  Slide(
+    transition: defaultTransition,
+    builder: (context) {
+      return ContentSlide(
+        content: Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(
+              color: Colors.white,
+              width: 1,
+            ),
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: SpriteWidget(naiveCollisionView100),
+        ),
+      );
+    },
+  ),
+  Slide(
+    transition: const SlickFadeTransition(
+      duration: Duration(milliseconds: 250),
+    ),
+    builder: (context) {
       return const ContentSlide(
         content: ProviderScope(
-          child: QuadtreeView(),
+          child: FadeIn(
+            child: QuadtreeView(),
+          ),
         ),
       );
     },
@@ -73,3 +116,89 @@ final collisionDetectionSlides = [
     ),
   ),
 ];
+
+final naiveCollisionView10 = NaiveCollisionView(10);
+final naiveCollisionView100 = NaiveCollisionView(100);
+
+class NaiveCollisionView extends NodeWithSize {
+  final int _numberOfObjects;
+
+  NaiveCollisionView(this._numberOfObjects) : super(const Size(1024, 768)) {
+    var random = Random();
+
+    for (int i = 0; i < _numberOfObjects; i++) {
+      _positions.add(
+        Offset(
+          random.nextDouble() * 1024,
+          random.nextDouble() * 768,
+        ),
+      );
+
+      _velocities.add(
+        Offset(
+          random.nextDouble() * 50 - 25,
+          random.nextDouble() * 50 - 25,
+        ),
+      );
+
+      _radii.add(
+        random.nextDouble() * 5 + 2,
+      );
+    }
+  }
+
+  final List<Offset> _positions = [];
+  final List<Offset> _velocities = [];
+  final List<double> _radii = [];
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    for (var i = 0; i < _numberOfObjects; i++) {
+      _positions[i] += _velocities[i] * dt;
+
+      // Check bounds.
+      if (_positions[i].dx < 0) {
+        _positions[i] = Offset(0, _positions[i].dy);
+        _velocities[i] = Offset(-_velocities[i].dx, _velocities[i].dy);
+      } else if (_positions[i].dx > 1024) {
+        _positions[i] = Offset(1024, _positions[i].dy);
+        _velocities[i] = Offset(-_velocities[i].dx, _velocities[i].dy);
+      }
+      if (_positions[i].dy < 0) {
+        _positions[i] = Offset(_positions[i].dx, 0);
+        _velocities[i] = Offset(_velocities[i].dx, -_velocities[i].dy);
+      } else if (_positions[i].dy > 768) {
+        _positions[i] = Offset(_positions[i].dx, 768);
+        _velocities[i] = Offset(_velocities[i].dx, -_velocities[i].dy);
+      }
+    }
+  }
+
+  @override
+  void paint(Canvas canvas) {
+    super.paint(canvas);
+
+    var circlePaint = Paint()..color = Colors.red;
+    var linePaint = Paint()..color = Colors.white24;
+
+    for (var i = 0; i < _numberOfObjects; i++) {
+      for (var j = 0; j < _numberOfObjects; j++) {
+        canvas.drawLine(
+          _positions[i],
+          _positions[j],
+          linePaint,
+        );
+      }
+    }
+
+    for (var i = 0; i < _numberOfObjects; i++) {
+      canvas.drawCircle(
+        _positions[i],
+        _radii[i],
+        circlePaint,
+      );
+    }
+  }
+}
--- packages/slick_slides/lib/slick_slides.dart
+++ packages/slick_slides/lib/slick_slides.dart
@@ -8,6 +8,7 @@ export 'src/transitions/fade.dart';
 export 'src/transitions/transition.dart';
 export 'src/widgets/bullets.dart';
 export 'src/widgets/colored_code.dart';
+export 'src/widgets/fade_in.dart';
 export 'src/widgets/gradient_text.dart';
 export 'src/deck/deck.dart';
 export 'src/deck/theme.dart';
